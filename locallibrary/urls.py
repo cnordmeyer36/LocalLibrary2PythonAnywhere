950736e7e7452afa844814176a6a91c5195e5ab4
cnordmeyer36 <cp.nordmeyer2003@gmail.com>
2025-06-18 16:25:59 -0500

    Update urls.py

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,15 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve  # <-- ADD THIS LINE
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('register.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
-settings.MEDIA_ROOT}), #serve media files when deployed
-re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
-settings.STATIC_ROOT}), #serve static files when deployed
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
