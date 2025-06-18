6bbc17fb0bc1ec878e8ca97fd0ab024d33817f3d
cnordmeyer36 <cp.nordmeyer2003@gmail.com>
2025-06-18 16:07:48 -0500

    Update urls.py

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -9,3 +9,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('register.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+re_path(r'^media/(?P<path>.*)$', serve, {'document_root':
+settings.MEDIA_ROOT}), #serve media files when deployed
+re_path(r'^static/(?P<path>.*)$', serve, {'document_root':
+settings.STATIC_ROOT}), #serve static files when deployed
